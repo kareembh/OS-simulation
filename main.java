1bc64f47ebf3f6d492d28802d06297743aa0e3fc
Kareem <kareemzx@hotmail.com>
2018-02-17 20:13:36 -0500

    Add files via upload

--- main.java
+++ main.java
@@ -18,24 +18,40 @@ public class main {
 		}
 	}
 	
-	public static void processInterupt() {
+	public static void timerInterupt() {
 		ready.add(cpuRunning);
 		cpuRunning = ready.remove();
 	}
 	
-
+	public static void requestIo(int deviceNum){
+		//the deviceNum is the device number associated with its position in the device Array.
+		
+	}
 	
-	
-	
+	public static void completionIo(int deviceNum){
+		//the deviceNum is the device number associated with its position in the device Array.
+		
+	}
 	
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayDeque<device> deviceArray = new ArrayDeque<device>();
+		
+		//Asks for number of devices
+		System.out.print("How many I/O devices will you need?");
+		int deviceNum= sc.nextInt();
+		
+		//creates multiple arrays based on user input. Not yet finished!
+		for(int i =0;i<deviceNum;i++){
+			//deviceArray.add(devices = new device());
+		}
 		
 		
 		//asks for inturrpt codes
-		Scanner sc = new Scanner(System.in);
 		System.out.print("enter a number");
 		int temp= sc.nextInt();
 		

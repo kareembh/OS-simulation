2fe15edf6e907cf189848a7f2a5823682fccd84c
Kareem <kareemzx@hotmail.com>
2018-02-18 16:08:51 -0500

    Add files via upload

--- device.java
+++ device.java
@@ -2,8 +2,14 @@ import java.util.ArrayDeque;
 
 public class device {
 
+	private int deviceNumber;
 	
 	int running;
+	
+	device(){
+		
+	}
 	static ArrayDeque<device> dReady = new ArrayDeque<device>();
+	
 	
 }
--- main.java
+++ main.java
@@ -8,7 +8,13 @@ public class main {
 	static ArrayDeque<Integer> ready = new ArrayDeque<Integer>();
 	static ArrayDeque<device> devices = new ArrayDeque<device>();
 	
+	//timer Interrupt method 15
+	public static void timerInterrupt() {
+		ready.add(cpuRunning);
+		cpuRunning = ready.remove();
+	}
 	
+	//Create a process method 80
 	public static void createProcess() {
 		processCounter++;
 		if(cpuRunning == processCounter){
@@ -18,19 +24,46 @@ public class main {
 		}
 	}
 	
-	public static void timerInterupt() {
-		ready.add(cpuRunning);
-		cpuRunning = ready.remove();
-	}
-	
+	//Request I/O method 70
 	public static void requestIo(int deviceNum){
 		//the deviceNum is the device number associated with its position in the device Array.
 		
 	}
 	
+	//Completion of I/O method 71
 	public static void completionIo(int deviceNum){
 		//the deviceNum is the device number associated with its position in the device Array.
 		
+	}
+	
+	//Wait system call method 64
+	public static void waitSysCall(){
+		
+	}
+	
+	//Signal waiting method 65
+	public static void signalWaiting(){
+		
+	}
+	
+	//Graceful shut down method 244
+	public static void graceful(){
+		
+	}
+	
+	//Status display method
+	public static void status(){
+		
+	}
+	
+	//Terminates process 81
+	public static void terminateProcess(){
+		
+	}
+	
+	//Ungraceful shutdown
+	public static void ungraceful(){
+		System.exit(0);
 	}
 	
 	
@@ -47,7 +80,8 @@ public class main {
 		
 		//creates multiple arrays based on user input. Not yet finished!
 		for(int i =0;i<deviceNum;i++){
-			//deviceArray.add(devices = new device());
+			deviceArray.add(new device());
+			System.out.println("this is device " + (i+1));
 		}
 		
 		
@@ -55,9 +89,54 @@ public class main {
 		System.out.print("enter a number");
 		int temp= sc.nextInt();
 		
-		while(temp!=1){
+		while(1<2){
 		     System.out.print("enter a number");
 		     temp = sc.nextInt();
+		     
+		     switch(temp) {
+		     case 01 :
+			        ungraceful();
+			        break;
+		     
+		     case 15 :
+		        timerInterrupt();
+		        break; 
+		     
+		     case 64 :
+		    	waitSysCall();
+		        break; 
+		        
+		     case 65 :
+		    	 signalWaiting();
+			     break; 
+		     
+		     case 70 :
+		    	 requestIo(deviceNum);
+		    	 break; 
+			        
+		     case 71 :
+		    	 completionIo(deviceNum);
+		    	 break; 
+			        
+		     case 80 :
+			     createProcess();
+			     break; 
+			        
+		     case 81 :
+		    	 terminateProcess();
+			     break; 
+			        
+		     case 244 :
+		    	 graceful();
+			     break; 
+			        
+		     case 255 :
+			     status();
+			     break; 
+			       
+		     default : // Optional
+		        // Statements
+		  }
 		}
 		
 	}
